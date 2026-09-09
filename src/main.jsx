0484dca88e50f54a5152f759e53a73a6f80590f7
RvRamzes <RvRamzes@gmail.com>
2026-09-09 23:19:52 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
 // Реєстрація Service Worker для роботи офлайн
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then((reg) => console.log('SW зареєстровано:', reg))
       .catch((err) => console.log('Помилка реєстрації SW:', err));
   });
